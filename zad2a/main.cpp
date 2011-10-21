9b3a7af8f653e5613d03920c5fc921c84c7db0b3
Piotr Kowalski <piotrek.kowalski@mchtr.pw.edu.pl>
2011-10-21 12:51:40 +0200

    rekurencyjna implementacja ciagu fibonacciego

--- zad2a/main.cpp
+++ zad2a/main.cpp
@@ -2,8 +2,21 @@
 
 using namespace std;
 
+int fib_rec(int nr) {
+    if(nr == 0) {
+        return 0;
+    }
+    if(nr == 1) {
+        return 1;
+    }
+    return fib_rec(nr-1) + fib_rec(nr-2);
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    for(int i=0; i < 10; i++) {
+        int result = fib_rec(i);
+        cout << "fib = " << result << endl;
+    }
     return 0;
 }
