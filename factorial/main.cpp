5fd8431c184fe45c562e9ccdd45190900f30860a
Piotr Kowalski <piotrek.kowalski@mchtr.pw.edu.pl>
2011-10-21 14:01:53 +0200

    factorial, increment

---
+++ factorial/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+// dlaczego nie dziala?
+int factorial(int nr)
+{
+    int result = 1;
+    for(int i=0; i<nr; ++i)
+    {
+        result = result*i;
+        //result *= 1;
+    }
+    return result;
+}
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
---
+++ increment/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i = 3;
+    int counter = 7;
+    cout << "i = " << i << endl;
+    //i += 1; //
+    //i = i+1; //
+    cout << "i++ = " << i++ << endl;
+    cout << "i = " << i << endl;
+    //cout << "++i = " << ++i << endl;
+    i += 2;
+    cout << "i = " << i << endl;
+    return 0;
+}
