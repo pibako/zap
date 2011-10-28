d6a363ca8eaaa84d53e54b8f7c41faa21a48fbef
Piotr Kowalski <piotrek.kowalski@mchtr.pw.edu.pl>
2011-10-28 13:10:54 +0200

    formatowanie

--- zad2a/main.cpp
+++ zad2a/main.cpp
@@ -2,36 +2,29 @@
 
 using namespace std;
 
-int fib_rec(int nr)
-{
-    if(nr == 0)
-    {
+int fib_rec(int nr) {
+    if(nr == 0) {
         return 0;
     }
-    if(nr == 1)
-    {
+    if(nr == 1) {
         return 1;
     }
     return fib_rec(nr-1) + fib_rec(nr-2);
 }
 
-int fib_iter(int nr)
-{
+int fib_iter(int nr) {
     int fib_2, fib_1, fib;
     fib_2 = 0;
     fib_1 = 1;
     fib = fib_2 + fib_1;
-    if(nr == 0)
-    {
+    if(nr == 0) {
         return 0;
     }
-    if(nr == 1)
-    {
+    if(nr == 1) {
         return 1;
     }
 
-    for(int i = 2; i<nr; i++)
-    {
+    for(int i = 2; i<nr; i++) {
         fib_2 = fib_1;
         fib_1 = fib;
         fib = fib_1 + fib_2;
@@ -39,8 +32,7 @@ int fib_iter(int nr)
     return fib;
 }
 
-int main()
-{
+int main() {
     //for(int i=0; i < 5; i++)
     //{
     //    int result = fib_iter(42);
@@ -48,8 +40,7 @@ int main()
     //}
 
     int i = 0;
-    while(i < 10)
-    {
+    while(i < 10) {
         int result = fib_iter(i);
         cout << "fib = " << result << endl;
         i += 2;
