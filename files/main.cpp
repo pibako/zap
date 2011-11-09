58fd6d9ab9bdeac0605b9525cdcb6fd51f49c78d
Piotr Kowalski <piotrek.kowalski@mchtr.pw.edu.pl>
2011-11-09 13:01:52 +0100

    input files

---
+++ files/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+bool is_letter(char c) {
+    if((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
+        return true;
+    }
+    return false;
+}
+
+bool is_digit(char c) {
+    if(c >= '0' && c <= '9') {
+        return true;
+    }
+    return false;
+}
+
+bool is_operation(char c) {
+    if(c == '+' || c == '-' || c == '*' || c == '/') {
+        return true;
+    }
+    return false;
+}
+
+int main()
+{
+    ifstream is("./data/chars.txt");
+
+    if (is.is_open()) {
+        cout << "Plik chars.txt istnieje" << endl;
+    } else {
+        cout << "Plik chars.txt nie istnieje" << endl;
+    }
+
+    while(is.good()) {
+        char c = char(is.get());
+        if (is.good()) {
+            cout << c;
+        }
+    }
+
+    cout << "Hello world! " << is_operation('+') << endl;
+    return 0;
+}
