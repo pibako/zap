7c4e4148509dfb88da98d1bef564a90e51ac82a1
Piotr Kowalski <piotrek.kowalski@mchtr.pw.edu.pl>
2011-11-09 14:08:14 +0100

    passing functions to arrays

---
+++ arrays_func/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+void init_array(int array[], int size) {
+    for(int i = 0; i < size; i++) {
+        array[i] = 1;
+    }
+}
+
+void print_array(int array[], int size) {
+    for(int i = 0; i < size; i++) {
+        cout << array[i] << ", ";
+    }
+    cout << endl;
+}
+
+void modify_var(double x) {
+    x = 10;
+}
+
+int main()
+{
+    int const size = 10;
+    int numbers[size];
+
+    print_array(numbers, size);
+
+    init_array(numbers, size);
+
+    print_array(numbers, size);
+
+    double num = 3;
+    cout << num << endl;
+    modify_var(num);
+    cout << num << endl;
+
+    return 0;
+}
