ea5755f71df308f42c8263055584c3077269f431
Piotr Kowalski <piotrek.kowalski@mchtr.pw.edu.pl>
2011-12-02 15:42:59 +0100

    Przyklady zarzadzania pamiecia

---
+++ mem/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+void swap(int *x, int *y) {
+    int tmp = *y;
+    *y = *x;
+    *x = tmp;
+}
+
+void swap2(int &x, int &y) {
+    int tmp = y;
+    y = x;
+    x = tmp;
+}
+
+int main()
+{
+    //int arr[10000000];
+    int *arr2 = new int[100000000];
+
+    int x = 1;
+    int y = 2;
+    cout << "x = " << x << " y = " << y << endl;
+    swap(&x,&y);
+    cout << "x = " << x << " y = " << y << endl;
+    swap2(x,y);
+    cout << "x = " << x << " y = " << y << endl;
+
+    int *m = new int;
+    int *n = new int;
+    *m = 5;
+    *n = 4;
+
+    cout << "m = " << *m << " n = " << *n << endl;
+    swap(m,n);
+    cout << "m = " << *m << " n = " << *n << endl;
+    swap2(*m,*n);
+    cout << "m = " << *m << " n = " << *n << endl;
+
+
+
+    int *a = new int;
+    *a = 10;
+    cout << "a = " << a << endl;
+    cout << "*a = " << *a << endl;
+    int b = 5;
+    cout << "b = " << b << endl;
+    cout << "&b = " << &b << endl;
+    a = &b;
+    cout << "a = " << a << endl;
+    cout << "*a = " << *a << endl;
+    cout << "&a = " << &a << endl;
+
+    return 0;
+}
---
+++ mem2/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+struct Point {
+    double *x;
+    double *y;
+};
+
+void swap(int *x, int *y) {
+    int tmp = *y;
+    *y = *x;
+    *x = tmp;
+}
+
+int main()
+{
+    Point p;
+    p.x = new double;
+    p.y = new double;
+
+    *(p.x) = 40;
+    *(p.y) = 50;
+
+    swap(p.x, p.y);
+
+    cout << *(p.x) << "  " << *(p.y) << endl;
+
+    Point *p_p = new Point;
+    (*p_p).x = new double;
+    //p_p->x = new double;
+
+    int arr[10];
+    int *arr2 = new int[10];
+
+    for(int i = 0; i < 10; i++) {
+        arr[i] = i;
+    }
+    for(int i = 0; i < 10; i++) {
+        cout << "arr[" << i << "] = " << arr[i] << endl;
+    }
+    for(int i = 0; i < 10; i++) {
+        cout << "arr[" << i << "] = " << *arr + i << endl;
+    }
+
+
+    return 0;
+}
